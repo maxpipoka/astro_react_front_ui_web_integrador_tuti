27c922a47576c8e62c963e040275d2e7c378db44
maxpipoka <martincardozo@gmail.com>
2025-01-22 22:32:04 -0300

    Actualizar dependencias de Astro y cambiar el adaptador a Vercel en la configuración

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel"; // Importa el adaptador de Vercel
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'server', // Necesario para adaptadores basados en servidores como Vercel
+  adapter: vercel(), // Cambia el adaptador a Vercel
 });
